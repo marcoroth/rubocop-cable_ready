2082b16bdc76de0b18a5b03ab7391a7fb4226878
Julian Rubisch <julian@julianrubisch.at>
2021-09-23 21:20:32 +0200

    Write unused call spec (#1)
    * Write unused call spec
    
    * run rubocop
    
    Co-authored-by: Marco Roth <marco.roth@intergga.ch>

---
+++ spec/rubocop/cop/cable_ready/unused_cable_ready_call_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::CableReady::UnusedCableReadyCall, :config do
+  it "registers an offense for an unused cable_ready call" do
+    expect_offense(<<~RUBY)
+      class Foo
+        include CableReady::Broadcaster
+
+        def bar
+          cable_ready
+          ^^^^^^^^^^^ unused `cable_ready` call
+        end
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense if cable_ready.broadcast is called" do
+    expect_no_offenses(<<~RUBY)
+      class Foo
+        include CableReady::Broadcaster
+
+        def bar
+          cable_ready["MyChannel"].morph({
+            selector: "#foo",
+            html: "<div></div>"
+          })
+          cable_ready.broadcast
+        end
+      end
+    RUBY
+  end
+end
