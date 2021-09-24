94255d15dbb72f4107427429b4e67957022d55a4
Julian Rubisch <julian@julianrubisch.at>
2021-09-24 09:53:27 +0200

    Include spec_helper manually

--- spec/rubocop/cop/cable_ready/broadcaster_controller_action_spec.rb
+++ spec/rubocop/cop/cable_ready/broadcaster_controller_action_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "spec_helper"
+
 RSpec.describe RuboCop::Cop::CableReady::BroadcasterControllerAction, :config do
   it "registers an offense for a controller mixing in Broadcaster" do
     expect_offense(<<~RUBY)
